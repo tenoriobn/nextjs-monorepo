b128e5dc37e6cd9c7266ab39e498f097ff0bea8d
Bruno Tenório <tenoriobn@gmail.com>
2024-11-06 16:20:28 -0300

    adicionando e configurando o ESLINT no projeto

--- packages/design-system/components/Text/index.tsx
+++ packages/design-system/components/Text/index.tsx
@@ -1,19 +1,19 @@
-import styled from 'styled-components'
-import React from 'react'
+import styled from "styled-components";
+import React from "react";
 
 const StyledTxt = styled.span`
   font-family: sans-serif;
 `;
 
 interface TextProps {
-  tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span';
+  tag: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
   children: React.ReactNode;
 }
 
-export function Text({ tag, children, ...props}: TextProps) {
+export function Text({ tag, children, ...props }: TextProps) {
   return (
     <StyledTxt as={tag} {...props}>
       {children}
     </StyledTxt>
-  )
+  );
 }
--- setup/eslint-commons/base.js
+++ setup/eslint-commons/base.js
@@ -3,5 +3,9 @@ module.exports = {
     "next",
     "eslint:recommended",
     "plugin:prettier/recommended"
-  ]
+  ],
+  "rules": {
+    "no-console": "error",
+    "@next/next/no-html-link-for-pages": "off",
+  }
 }
