06cbfa56269af87946aeef2bb5662d50e151e84d
Bruno Tenório <tenoriobn@gmail.com>
2024-11-06 15:52:15 -0300

    corrigindo versões anteriores

---
+++ projects/web-public/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { sum } from "@alura/utils/math/sum";
+import { Text } from "@alura/design-system/components/Text";
+
+export default function HomeScreen() {
+  return (
+    <main>
+      <Text tag="h1">Home</Text>
+      <Text tag="p">
+        Importando modulo local: @alura/utils/math/sum sum(2,2): {sum(2, 2)}
+      </Text>
+    </main>
+  );
+}
